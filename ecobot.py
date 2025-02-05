0918eea7d7e6445ffbb6b0310a3b4418814f65af
djmejiar23 <djmejiar23@gmail.com>
2025-02-04 20:40:50 -0500

    Add files via upload

---
+++ ecobot.py
@@ -0,0 +1,29 @@
+import discord
+from discord.ext import commands
+import random
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix ='!',intents = intents)
+
+#retoss
+
+retos = [
+    "No utilisar plasticos por 1 semana.",
+    "No utilizar el carro por un día.",
+    "Plantar un árbol o una planta.",
+    "Recoge la basura.",
+    "Apaga las luces cuando salgas de una habitación.",
+]  
+
+@bot.command()
+async def reto(ctx):
+    reto_aleatorio = random.choice(retos) 
+    await ctx.send(f"¡Tu reto ambiental es: {reto_aleatorio}")
+
+@bot.event
+async def on_ready():
+    print(f'Bot conectado como {bot.user}')
+
+bot.run('MTMyODExMDU0MDM1NDA5NzI3NA.GDIMSh.8JoXb2wgtwskGZgFQVr9Nqrt_t0ft32JlWfK1I')  
